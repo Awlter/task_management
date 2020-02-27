313924397b93d006042a471bce073ae7178ff370
wangxuefei <xuefeiwang@jinshuju.net>
2020-02-27 10:09:10 +0800

    add sessions controller and routes for new create and destroy for logging system

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  post 'logout', to: 'sessions#destroy'
+
   get 'backlog/list', to: 'tasks#index'
   resources :tasks, only: %i[create update destroy]
 end

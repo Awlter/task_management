69f23ffd24de45ee5ff82a8a02f1a8cbd5dc41e5
wangxuefei <xuefeiwang@jinshuju.net>
2020-02-27 11:22:46 +0800

    enable user to log in with an exsiting account and implement the sanity check

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,34 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
+  skip_before_action :authorized, only: %i[new create]
+  before_action :already_login?, only: :new
+
   def new; end
 
-  def create; end
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user&.authenticate(params[:password])
+      flash[:success] = "Hi, #{@user.username}, nice to see you again~"
+      session[:user_id] = @user.id
+      redirect_to backlog_list_path
+    else
+      flash[:danger] = 'User does not exist or the password is not correct'
+      redirect_to '/login'
+    end
+  end
 
-  def destroy; end
+  def destroy
+    session.delete :user_id
+    redirect_to login_path
+  end
+
+  private
+
+  def already_login?
+    if logged_in?
+      flash[:warning] = 'Alreadt Logged In.'
+      redirect_to backlog_list_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  post 'logout', to: 'sessions#destroy'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
 
   get 'backlog/list', to: 'tasks#index'
   resources :tasks, only: %i[create update destroy]
